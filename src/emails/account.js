0837c644afe333fd418eaec0eb3120d1d6cede71
Bharat1002 <bharatjc1002@gmail.com>
2022-10-10 18:42:53 +0530

    adding email send functionality

---
+++ src/emails/account.js
@@ -0,0 +1,35 @@
+const mailgun = require("mailgun-js");
+require('dotenv').config();
+
+const DOMAIN = process.env.Domain_name;
+const api_key = process.env.MAIL_GUN_API_KEY;
+const mg = mailgun({apiKey: api_key, domain: DOMAIN});
+
+const sendWelcomeEmail = (email, name) => {
+    mg.messages().send({
+        from: process.env.MY_MAIL,
+        to: email,
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    }, function (error, body){
+        console.log(body);
+        console.log(error);
+    });
+}
+
+const sendCancelationEmail = (email, name) => {
+    mg.messages().send({
+        from: process.env.MY_MAIL,
+        to: email,
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    }, function (error, body){
+        console.log(body);
+        console.log(error);
+    });
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
